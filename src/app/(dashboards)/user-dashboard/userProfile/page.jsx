64525c7bde49ccacfd9e4ea33603be9d918a1438
rasel3980 <rkrasel3980@gmail.com>
2025-12-01 14:49:33 +0600

    fixed error

--- src/app/(dashboards)/user-dashboard/userProfile/page.jsx
+++ src/app/(dashboards)/user-dashboard/userProfile/page.jsx
@@ -1,29 +1,16 @@
 'use client'
-export const dynamic = "force-dynamic";
-import { useSession } from 'next-auth/react';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const Profile = () => {
-    const {data:session , status} = useSession();
-    const router = useRouter()
-    if(status === "loading"){
-        return <p>loading......</p>
-    }
-
-    if(!session?.user){
-        router.push('/')
-        return null
-    }
     return (
         <div className='flex justify-center items-center flex-col mt-12 border w-1/3 mx-auto py-5'>
             <div>
-                <Image className='rounded-full' alt='profile' width={150} height={150} src={session?.user?.image}></Image>
+                <Image className='rounded-full' alt='profile' width={150} height={150} src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D"></Image>
             </div>
             <div className='mt-10'>
-                <h1 className='font-bold text-lg'>{session?.user?.name}</h1>
-                <p>{session?.user?.email}</p>
+                <h1 className='font-bold text-lg'>Rasel</h1>
+                <p>Test@gmail.com</p>
             </div>
         </div>
     );
