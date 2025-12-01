133e271e17c36b9cd44d6c6f182c56c3f07ffd8b
rasel3980 <rkrasel3980@gmail.com>
2025-12-01 15:22:07 +0600

    vercel deployment  error fixed

--- src/app/(userInfo)/profile/page.jsx
+++ src/app/(userInfo)/profile/page.jsx
@@ -1,18 +1,22 @@
 'use client'
+export const dynamic = "force-dynamic";
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import React from 'react';
 
 const UserProfile = () => {
-    const session = useSession()
+    const {data:session,status} = useSession()
+    if(status === "loading"){
+        return <p>Loading........</p>
+    }
     return (
         <div className='flex justify-center items-center flex-col mt-12 border w-1/3 mx-auto py-5'>
             <div>
-                <Image className='rounded-full' alt='profile' width={150} height={150} src={session.data.user.image}></Image>
+                <Image className='rounded-full' alt='profile' width={150} height={150} src={session.user.image}></Image>
             </div>
             <div className='mt-10'>
-                <h1 className='font-bold text-lg'>{session.data.user.name}</h1>
-                <p>{session.data.user.email}</p>
+                <h1 className='font-bold text-lg'>{session.user.name}</h1>
+                <p>{session.user.email}</p>
             </div>
         </div>
     );
