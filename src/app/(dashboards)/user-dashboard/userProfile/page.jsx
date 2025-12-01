05ace114ebf47be5a9759289a8bc8a29e0caa08f
rasel3980 <rkrasel3980@gmail.com>
2025-12-01 14:15:32 +0600

    error solved

--- src/app/(dashboards)/user-dashboard/userProfile/page.jsx
+++ src/app/(dashboards)/user-dashboard/userProfile/page.jsx
@@ -4,15 +4,16 @@ import Image from 'next/image';
 import React from 'react';
 
 const Profile = () => {
-    const session = useSession()
+    const session = useSession();
+    const user = session?.data?.user
     return (
         <div className='flex justify-center items-center flex-col mt-12 border w-1/3 mx-auto py-5'>
             <div>
-                <Image className='rounded-full' alt='profile' width={150} height={150} src={session.data.user.image}></Image>
+                <Image className='rounded-full' alt='profile' width={150} height={150} src={user?.image}></Image>
             </div>
             <div className='mt-10'>
-                <h1 className='font-bold text-lg'>{session.data.user.name}</h1>
-                <p>{session.data.user.email}</p>
+                <h1 className='font-bold text-lg'>{user?.name}</h1>
+                <p>{user?.email}</p>
             </div>
         </div>
     );
