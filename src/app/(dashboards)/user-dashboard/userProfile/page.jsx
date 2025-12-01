44a0b2baba503115907aaa4e99dc46c078e39184
rasel3980 <rkrasel3980@gmail.com>
2025-12-01 14:39:30 +0600

    fixed error

--- src/app/(dashboards)/user-dashboard/userProfile/page.jsx
+++ src/app/(dashboards)/user-dashboard/userProfile/page.jsx
@@ -1,19 +1,29 @@
 'use client'
+export const dynamic = "force-dynamic";
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const Profile = () => {
-    const session = useSession();
-    const user = session?.data?.user
+    const {data:session , status} = useSession();
+    const router = useRouter()
+    if(status === "loading"){
+        return <p>loading......</p>
+    }
+
+    if(!session?.user){
+        router.push('/')
+        return null
+    }
     return (
         <div className='flex justify-center items-center flex-col mt-12 border w-1/3 mx-auto py-5'>
             <div>
-                <Image className='rounded-full' alt='profile' width={150} height={150} src={user?.image}></Image>
+                <Image className='rounded-full' alt='profile' width={150} height={150} src={session?.user?.image}></Image>
             </div>
             <div className='mt-10'>
-                <h1 className='font-bold text-lg'>{user?.name}</h1>
-                <p>{user?.email}</p>
+                <h1 className='font-bold text-lg'>{session?.user?.name}</h1>
+                <p>{session?.user?.email}</p>
             </div>
         </div>
     );
