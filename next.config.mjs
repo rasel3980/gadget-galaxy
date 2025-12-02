496cb209cee757b4f1013cd8be98a87de3bbce71
rasel3980 <rkrasel3980@gmail.com>
2025-12-03 00:38:29 +0600

    deployment error solved

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'extremegadgets.com.bd',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
       },
       {
